16c8c392f38fba04c88979b3cef919398948f863
Retky <jose.arredondo.a@outlook.com>
2022-02-04 16:12:24 -0600

    Change content of nodes in "dom.js"

--- js/dom.js
+++ js/dom.js
@@ -2,6 +2,11 @@
 /* 02 All Elements ("<div>, <p>, ...") in the DOM are Objects. */
 /* 03 We can change elements inside nodes, the parents of the elements. */
 /* 04 "getElementsBy..." calls an element. */
-let contDivs = document.getElementsByClassName("container")
-let myH2 = contDivs[1].getElementsByTagName("h2")
-myH2[0].innerHTML = "Hello!"
+let contDivs = document.getElementsByClassName("container");
+let myH2 = contDivs[1].getElementsByTagName("h2");
+let myBody = document.getElementsByTagName("body");
+/* 05 "innerHTML" returns the HTML content of an element. */
+//myBody[0].innerHTML = "<p>I am a paragraph tag</p>"
+let title = document.getElementById('pageTitle');
+/* 06 Can change the content of nodes. */
+//title.textContent = "Hello World!"
