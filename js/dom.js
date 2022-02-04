f8a5486a72ff468f57216aea9a15c5bf32586897
Retky <jose.arredondo.a@outlook.com>
2022-02-04 16:02:36 -0600

    getElements with the DOM in "dom.js"

--- js/dom.js
+++ js/dom.js
@@ -1,2 +1,7 @@
 /* 01 "DOM" stands for Document Object Model. */
 /* 02 All Elements ("<div>, <p>, ...") in the DOM are Objects. */
+/* 03 We can change elements inside nodes, the parents of the elements. */
+/* 04 "getElementsBy..." calls an element. */
+let contDivs = document.getElementsByClassName("container")
+let myH2 = contDivs[1].getElementsByTagName("h2")
+myH2[0].innerHTML = "Hello!"
